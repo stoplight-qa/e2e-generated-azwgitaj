0687e630bb9fef00c732d75a46d6986ef9cd6f84
qa-mssdg-0-clos@example.com <qa-mssdg-0-clos@example.com>
2023-01-31 14:53:36 -0500

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjk0MzEx/spectral.js?branch_slug=master&token=1d247b8a-15c7-4893-b0b4-594786334d95";
+export default { extends: ruleset };
